dda98147581c0fe2d43b4d8f90a289b323a03046
Eskimo <16081627@hdu.edu.cn>
2020-02-05 16:26:19 +0800

    添加icon 和iconPosition属性

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,18 @@
 <template>
-  <button class="f-button">按钮</button>
+  <button class="f-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`" />
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["icon", "iconPosition"] //left right
+};
 </script>
 
 
@@ -15,14 +24,19 @@ export default {};
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
-  &:hover {
-    border-color: var(--button-color-hover);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  &:hover {border-color: var(--button-color-hover);}
+  &:active {background: var(--button-active-bg);}
+  &:focus {outline: none;}
+  > .icon {order: 1; margin-right: .3em;}
+  > .content {order: 2;}
+  &.icon-right {
+    > .icon {order: 2;  margin-left: .3em; margin-right: 0;}
+    > .content {order: 1;}
   }
-  &:active {
-    background: var(--button-active-bg);
-  }
-  &:focus {
-    outline: none;
-  }
+ 
 }
 </style>
